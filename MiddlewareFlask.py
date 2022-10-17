c83fc779b0c8ab08084142ff69ce8322d3fddcf6
danielglez4234 <gnzlzdaniel.task@gmail.com>
2022-10-17 17:44:27 +0100

    add yaml and create _row_data function

--- MiddlewareFlask.py
+++ MiddlewareFlask.py
@@ -1,73 +1,195 @@
 import json
 import urllib.parse
 import requests
-import os
+import yaml
 from flask import Flask, request, jsonify, Response
-
-
-hostName = "161.72.123.165"
-serverPort = 8080
 
 app = Flask(__name__)
 
 
-@app.route("/subscriptions", methods=['POST'])
-def subscribe():
-    data = urllib.parse.quote(request.data.decode('utf-8'), safe='\"\n\t {}:,[]\\/$%')
-    json_post = json.JSONDecoder().decode(data)
-    notification = json_post['notification']
-    if 'httpCustom' in notification.keys():
-        if 'headers' in notification['httpCustom']:
-            notification['httpCustom']['headers']['destPage'] = notification['httpCustom']['url']
-        else:
-            notification['httpCustom']['headers']['destPage'] = notification['httpCustom']['url']
-    else:
-        new_notification = {"httpCustom": {
-            "url": notification['http']['url'],
+def get_config(path):
+    with open(path, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+
+config = get_config("config.yaml")['iot']
+HOSTNAME = config['server']['hostname']
+PORT = config['server']['port']
+SUBSCRIPTION_TARGET = config['server']['subscription_target']
+NOTIFICATION_TARGET = config['server']['notification_target']
+COMPONENTS_PREFIX = config['notification_prefix']['component']
+MAGNITUDE_PREFIX = config['notification_prefix']['magnitude']
+VALUE_PREFIX = config['notification_prefix']['value']
+
+
+# ROWS EXAMPLE
+# {
+#     "InternalName": "6_IOT_VAL",  # siempre igual
+#     "Type": "FieldsGroup",
+#     "FieldsGroupValue": [
+#         {
+#             "Row": [
+#                 {
+#                     "InternalName": "3_GC_IOT_humidity",
+#                     "Type": "Text",
+#                     "TextValue": "Valooooor"
+#                 },
+#                 {
+#                     "InternalName": "3_GC_IOT_WeatherStation",
+#                     "Type": "Text",
+#                     "TextValue": "Valooooor"
+#                 },
+#                 {
+#                     "InternalName": "3_GC_IOT_Valor",
+#                     "Type": "Decimal",
+#                     "TextValue": "Valooooor"
+#                 }
+#             ],
+#             "Row": [
+#                 {
+#                     "InternalName": "3_GC_IOT_NombreComponente",
+#                     "Type": "Text",
+#                     "TextValue": "Valooooor"
+#                 },
+#             ],
+#             "Row": [
+#                 {
+#                     "InternalName": "3_GC_IOT_Valor",
+#                     "Type": "Decimal",
+#                     "TextValue": "Valooooor"
+#                 }
+#             ]
+#         }
+#     ]
+# }
+
+
+# 
+# Replace %xx escapes with their single-character equivalent.
+# 
+def unquote(elements):
+    parse = {}
+    for element in elements.keys():
+        if element:
+            parse[element] = urllib.parse.unquote(elements[element])
+    return parse
+
+
+# 
+# create attributes data rows
+# 
+def create_row():
+    pass
+
+
+def create_attr_data(notification_data):
+    notification_model = {
+        "InternalName": "6_IOT_VAL",
+        "Type": "FieldsGroup",
+        "FieldsGroupValue": []
+    }
+    print(notification_data)
+    #
+    #  create arrange of components attributes
+    #
+    notification_model["FieldsGroupValue"].append(
+        create_row()
+    )
+
+    return notification_model
+
+
+# 
+# create new notification object if it doesn't already exist
+# 
+def create_notification(notification):
+    return {
+        "httpCustom": {
+            "url": NOTIFICATION_TARGET,
             "headers": {
                 "desPage": notification['http']['url']
             },
             'method': 'POST'
-        }}
-        json_post['notification'] = new_notification
+        }
+    }
 
-    notification['httpCustom']['url'] = "http://%s:%s/notification" % (hostName, serverPort)
+
+# TEST
+@app.route("/subscriptions", methods=['GET'])
+def get_data():
+    print(HOSTNAME)
+    print(PORT)
+    print(SUBSCRIPTION_TARGET)
+    r = requests.get(SUBSCRIPTION_TARGET, headers=request.headers)
+    return r.text, r.status_code
+
+
+# 
+# subscriptions POST route
+# 
+@app.route("/subscriptions", methods=['POST'])
+def subscribe():
+    data = urllib.parse.quote(request.data.decode('utf-8'), safe='\"\n\t {}:,[]\\/$%')
+
+    json_post = json.JSONDecoder().decode(data)
+    notification = json_post['notification']
+
+    if 'httpCustom' in notification.keys():
+        notification['httpCustom']['headers']['destPage'] = notification['httpCustom']['url']
+    else:
+        json_post['notification'] = create_notification(notification)
+
     print(json_post)
-    r = requests.post('http://orm-viotserver:1026/v2/subscriptions', json=json_post, headers=request.headers)
-    resp = Response("Example")
-    resp.headers = r.headers.items()
-    resp.status_code = r.status_code
-    resp.da = r.text
+    r = requests.post(SUBSCRIPTION_TARGET, json=json_post, headers=request.headers)
+
     return Response(json.dumps(r.text), status=r.status_code, mimetype='application/json', headers=r.headers.items())
 
 
+# 
+# notification POST route
+# 
 @app.route("/notification", methods=['POST'])
 def notify():
+    print("---------------------------------------------------------")
     data = urllib.parse.unquote(request.data.decode('utf-8'))
-    headers = {}
-    for header in request.headers.keys():
-        headers[header] = urllib.parse.unquote(request.headers[header])
-    print(headers)
+
+    # required object (Destpage)
+    headers = unquote(request.headers)
     dest_page = headers.pop("Destpage")
     json_post = json.JSONDecoder().decode(data)
-    print(json_post['PanelFields'][1])
-    r = requests.post(dest_page, json=json_post, headers={"apikey": headers['Apikey'], "userdata": headers['Userdata']})
-    return "200"
+
+    print("--------------------------------------------------------")
+    print(json_post)
+    # create response object
+    # json_response = createAttrData(json_post)
+
+    # r = requests.post(dest_page, json=json_post, headers={"apikey": headers['Apikey'], "userdata": headers['Userdata']})
+    # return json_response, r.status_code
 
 
+# 
+# subscriptions DELETE by id route
+# 
 @app.route("/subscriptions/<subscription_id>", methods=['DELETE'])
 def delete(subscription_id):
-    r = requests.delete('http://orm-viotserver:1026/v2/subscriptions/'+subscription_id)
+    r = requests.delete(SUBSCRIPTION_TARGET + subscription_id)
     return r.text, r.status_code
 
 
+# 
+# subscriptions DELETE by id with body route
+# 
 @app.route("/subscriptions", methods=['DELETE'])
 def delete_with_body():
     id = request.json['id']
-    r = requests.delete('http://orm-viotserver:1026/v2/subscriptions/'+id)
+    r = requests.delete(SUBSCRIPTION_TARGET + id)
     return r.text, r.status_code
 
 
+# 
+# test dummy
+# 
 @app.route("/dummy", methods=['POST'])
 def dummy():
     data = urllib.parse.unquote(request.data.decode('utf-8'))
@@ -76,4 +198,4 @@ def dummy():
 
 
 if __name__ == "__main__":
-    app.run(threaded=True, host=hostName, port=serverPort)
+    app.run(threaded=True, host=HOSTNAME, port=PORT)
